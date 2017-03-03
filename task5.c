8b4fe09ab82f3c2f5449a692eba7c5dd4afa926f
snowymountain <stay1996strongqq1@gmail.com>
2017-03-03 22:58:36 +0300

    first commit

---
+++ task5.c
@@ -0,0 +1,71 @@
+
+
+#include <stdio.h>
+
+#include <string.h>
+
+
+
+
+
+int main()
+
+{
+
+	FILE *in, *out;
+
+	char inputString[256];
+
+	int length;
+
+	in = fopen("in.txt", "rt");
+
+	out = fopen("out.txt", "wt");
+
+	if (in == NULL)
+
+	{
+
+		printf("File couldn't be open.\n");
+
+		return 1;
+
+	}
+
+
+
+	while (!feof(in))
+
+	{
+
+		fgets(inputString, 256, in);
+
+		
+
+		length = strlen(inputString);
+
+		for (int i = 0; i < length; i++)
+
+		{
+
+			if (inputString[i] == ' ')
+
+				inputString[i] = ',';
+
+		}
+
+		fputs(inputString, out);
+
+	}
+
+
+
+	fclose(in);
+
+	fclose(out);
+
+	
+
+	return 0;
+
+}
